e86126f3cf820410fadf3f64329d6a11ddff287f
Lance-Aldrin-Lu <110884885+Lance-Aldrin-Lu@users.noreply.github.com>
2024-12-05 20:36:05 +0800

    Update IE 142_Final.py

--- IE 142_Final.py
+++ IE 142_Final.py
@@ -2,7 +2,7 @@ import cvxpy as cp
 import numpy as np
 import pandas as pd
 
-file_path = "C:\\Users\\Lance Aldrin Lu\\Documents\\Python Programming\\CS 11 Crazy\\IE142test.csv"
+file_path = "C:\\Users\\Lance Aldrin Lu\\Documents\\Python Programming\\CS 11 Crazy\\IE 142 Stock Data Sheet - Lance_Copy.csv"
 df = pd.read_csv(file_path)
 returns = df.to_numpy()
 
@@ -13,35 +13,53 @@ i = len(expected_returns)                       # Number of stocks to analyze
 p = cp.Variable(i)                              # proportion i for each stock i
 
 
-# Stock proportion naming
-p1 = p[0]
-p2 = p[1]
-p3 = p[2]
-p4 = p[3]
-
 
 portfolio_return = p @ expected_returns          # Summation of (p_i * r_i) for all i
 portfolio_var = cp.quad_form(p, cov_matrix)      # [theta Transpose][phi][theta]
 
 
 # For the whole Portfolio constraint
-max_volatility = 0.015
-max_port_var = max_volatility**2
+max_volatility_annual = 0.1
+max_volatility_daily = max_volatility_annual / np.sqrt(252)
+max_port_var = max_volatility_daily**2
 
 
-# For the Stock i constraint
-var_s1 = cov_matrix[0, 0]
-max_var_s1 = 0.005
+# For the Stock i constraints
+var_s11 = cov_matrix[11, 11]
+max_volatility_s11_annual = 0.01
+max_volatility_s11_daily = max_volatility_s11_annual / np.sqrt(252)
+max_var_s11 = max_volatility_s11_daily**2
+
+var_s14 = cov_matrix[14, 14]
+max_volatility_s14_annual = 0.02
+max_volatility_s14_daily = max_volatility_s14_annual / np.sqrt(252)
+max_var_s14 = max_volatility_s14_daily**2
 
 
 constraints = [
                 cp.sum(p) == 1,                   # Total Probability Rule (and Max Budget)
                 portfolio_var <= max_port_var,    # Max Volatality of the Whole Portfolio
-                p1**2 * var_s1 <= max_var_s1,     # Max Volatility for stock i (currently set to limit stock 1)
-                p2 >= 0.2,                        # Lower limit for a stock i (currently set to limit stock 2)
-                p2 <= 0.3,                        # Upper limit for a stock i (currently set to limit stock 2)
+                
                 p >= 0                            # Non-negativity
 ]
+
+"""
+Contraints Pool:
+p[11]**2 * var_s11 <= max_var_s11,   # Max Volatility for stock i (currently set to limit stock 11)
+p[14]**2 * var_s14 <= max_var_s14,   # Max Volatility for stock i (currently set to limit stock 14)
+p[0] >= 0.12,                        # Lower limit for a stock i
+
+p[0] <= 0,                       # Upper limit for a stock i
+p[3] <= 0,                       # Upper limit for a stock i
+p[8] <= 0,                       # Upper limit for a stock i
+p[9] <= 0,                       # Upper limit for a stock i
+p[12] <= 0,                      # Upper limit for a stock i
+p[16] <= 0,                      # Upper limit for a stock i
+p[18] <= 0,                      # Upper limit for a stock i
+
+
+"""
+
 
 objective_function = cp.Maximize(portfolio_return)      # Objective function
 
@@ -50,8 +68,22 @@ problem = cp.Problem(objective_function,
 
 
 problem.solve(solver="ECOS")
-print("Solver: ECOS")
-print("Optimal Weights:", p.value)
+# 
+
+print("Optimal Proportions:")
+
+for stock in range(0, i):
+    print("Stock " + str(stock) + ": " + str(p[stock].value * 100))
+    
+
+#  + "   " + str(expected_returns[stock])
+
+print("")
+
 print("Optimal Portfolio Return:", portfolio_return.value)
 print("Optimal Portfolio Variance:", portfolio_var.value)
-print()
+
+print("")
+
+for cons in range(0, len(constraints)):
+    print("Dual Constraint " + str(cons) + ": " + str(problem.constraints[cons].dual_value))
